8229d21a220276235c82a14855924a2e63f35aa6
joseph0x45 <zozozozeph@gmail.com>
2024-08-21 22:25:52 +0000

    generate test suite run report

--- python/lib.py
+++ python/lib.py
@@ -4,6 +4,62 @@
 from urllib.parse import urlparse, parse_qs
 import importlib
 
+report_template_upper = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centered Table with Status Colors</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .status {
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+            display: inline-block;
+        }
+        .passed {
+            background-color: green;
+        }
+        .failed {
+            background-color: red;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Status</th>
+        </tr>
+"""
+
+report_template_lower = """
+  </table>
+</body>
+
+</html>
+"""
+
 
 def load_suite(name):
     try:
@@ -17,6 +73,20 @@ def load_suite(name):
         return None
 
 
+def generate_html_report(results) -> str:
+    report_table = ""
+    for k, v in results.items():
+        report_table = f"""
+            {report_table}
+            <tr>
+                <td>{k}</td>
+                <td><span class="status {'passed' if v else 'failed'}"></span></td>
+            </tr>
+        """
+    full_report = f"{report_template_upper}{report_table}{report_template_lower}"
+    return full_report
+
+
 def run_suite(suite_name):
     """
     Run the suite corresponding to the passed name if exists
@@ -27,8 +97,13 @@ def run_suite(suite_name):
         print("Suite not found")
         return
     print(f"About to run suite: {suite_name}")
+    results = {}
     for test_case in suite["test_cases"]:
-        test_case["fn"]()
+        result: bool = test_case["fn"]()
+        results[test_case["name"]] = result
+    report = generate_html_report(results)
+    with open(f"{suite_name}.html", "w") as file:
+        file.write(report)
 
 
 def get_reports():
